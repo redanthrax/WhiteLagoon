c41c0bf09a821897d9eea64ea9d2e033999ae597
ggagnon <ggagnon@REDRAZER>
2024-04-22 11:59:28 -0700

    Add project files.

---
+++ WhiteLagoon.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace WhiteLagoon.Web.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
